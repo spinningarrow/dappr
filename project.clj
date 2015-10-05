0a251026017a1d134df0ca10e9ed1392bc4199a3
Sahil Bajaj <sahil29@gmail.com>
2015-10-05 16:46:04 +0800

    Add ISC license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject dappr "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "ISC License"
+            :url "https://opensource.org/licenses/ISC"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clj-http  "2.0.0"]
                  [org.clojure/data.json  "0.2.6"]
